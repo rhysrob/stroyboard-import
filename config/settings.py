edf9c8239fab7818abaaea914f5912c13dcd1c83
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-12-12 11:10:36 +0000

    Add collectstatic

--- config/settings.py
+++ config/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
