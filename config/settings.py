a413ceced62395c19737c98aa86024b8d58a4442
Rhys Roberts <rhys.roberts@wjec.co.uk>
2023-03-02 11:39:24 +0000

    add localhost to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 DEPLOYED = env.bool("DEPLOYED")
 
 # Application definition
